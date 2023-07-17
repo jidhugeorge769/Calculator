263e4267db1907bc34a4eee13242ec0b4559dcd1
jidhu george <jidhugeorge769@gmail.com>
2023-07-17 15:02:26 +0530

    first commit

---
+++ calcc.js
@@ -0,0 +1,20 @@
+function displayData(num) {
+
+    result.value=result.value+num
+    
+}
+
+function evaluateEx() {
+
+    result.value=eval(result.value)    
+    
+}
+function allclear(){
+
+    result.value=""
+}
+function backSpace(){
+
+      result.value=result.value.slice(0,-1)
+
+  }
